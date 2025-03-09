b6c1b99551ec1637a6705d2173186209acac86bc
belloumyacine <bandartv72@gmail.com>
2025-03-09 23:45:54 +0100

    Update create mavgraĥ.kt

--- create mavgraĥ.kt
+++ create mavgraĥ.kt
@@ -5,12 +5,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.imatah.presentation.AddReportScreen
+import com.example.imatah.presentation.HomeScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "home") {
         composable("home") {
-            // HomeScreen content
+            HomeScreen(navController = navController)
         }
         composable("add_report") {
             AddReportScreen(navController = navController, onBack = { navController.popBackStack() })
